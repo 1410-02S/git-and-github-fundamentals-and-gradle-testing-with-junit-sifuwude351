4da29dee8669c1f20e4102348b9292cf823e7a9b
sifuwude351 <aeggers131@gmail.com>
2023-01-20 11:45:50 -0700

    blarg

--- src/main/java/com/example/project/Hello.java
+++ src/main/java/com/example/project/Hello.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class Hello {
 
 	public static void main(final String[] args) {
-		System.out.print("Hello world!");
+		System.out.println("Hello world!");
 	}
 
 }
